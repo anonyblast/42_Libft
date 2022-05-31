96b9d7644eb2dbc82eaa91f80dca73362bf9c467
gusluiz- <gusluiz-@student.42sp.org.br>
2022-05-31 17:07:04 +0200

    Create a Header File with string functions prototypes (1 or 0)

---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusluiz- <gusluiz-@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 03:36:15 by gusluiz-          #+#    #+#             */
+/*   Updated: 2022/05/31 17:04:45 by gusluiz-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+// Includes
+# include <stdio.h>
+# include <stdlib.h>
+
+// String Functions (Return 1 if TRUE or 0 if FALSE)
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+
+#endif

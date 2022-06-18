75ae4b6a0f896c6b98160c1168a1226b3b35d7d9
gusluiz- <gusluiz-@student.42sp.org.br>
2022-06-18 16:42:44 +0200

    Adding ft_itoa and modifying libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gusluiz- <gusluiz-@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 03:36:15 by gusluiz-          #+#    #+#             */
-/*   Updated: 2022/06/18 16:39:32 by gusluiz-         ###   ########.fr       */
+/*   Updated: 2022/06/18 16:42:23 by gusluiz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s);
 char	**ft_split(char const *str, char c);
+char	*ft_itoa(int n);
 
 #endif

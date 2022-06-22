9132a367e514ab27df5c7288f21cd5d18e095dd0
gusluiz- <gusluiz-@student.42sp.org.br>
2022-06-22 19:30:11 +0200

    Adding ft_putchar_fd and modifying libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gusluiz- <gusluiz-@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 03:36:15 by gusluiz-          #+#    #+#             */
-/*   Updated: 2022/06/22 19:23:39 by gusluiz-         ###   ########.fr       */
+/*   Updated: 2022/06/22 19:28:19 by gusluiz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ char	**ft_split(char const *str, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
 
 #endif
